d53ca4d380773d46fd876b8e015af9e92ec175ab
Ryan <tentyrisd@gmail.com>
2025-10-18 08:15:11 +0200

    Bonus

--- ft_lstadd_back_bonus.c
+++ ft_lstadd_back_bonus.c
@@ -10,3 +10,19 @@
 /*                                                                            */
 /* ************************************************************************** */
 
+void    ft_lstadd_back(t_list **lst, t_list *new)
+{
+    t_list    *last;
+
+    if (!lst || !new)
+        return ;
+    if (*lst == NULL)
+        *lst = new;
+    else
+    {
+        last = *lst;
+        while (last->next)
+            last = last->next;
+        last->next = new;
+    }
+}
---
+++ ft_lstsize_bonus.c
@@ -0,0 +1,11 @@
+int    ft_lstsize(t_list *lst)
+{
+    int    count = 0;
+
+    while (lst)
+    {
+        count++;
+        lst = lst->next;
+    }
+    return (count);
+}
