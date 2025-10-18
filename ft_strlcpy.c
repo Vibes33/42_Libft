3557e6252f247c1754effc464d6a7fb097990734
Ryan Delepine <rydelepi@c2r3p1.42lehavre.fr>
2025-10-18 09:19:33 +0200

    hello2

--- ft_strlcpy.c
+++ ft_strlcpy.c
@@ -6,7 +6,7 @@
 /*   By: rydelepi <rydelepi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/15 09:30:22 by rydelepi          #+#    #+#             */
-/*   Updated: 2025/10/17 09:03:16 by rydelepi         ###   ########.fr       */
+/*   Updated: 2025/10/18 09:10:52 by rydelepi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,6 +16,7 @@ size_t	strlcpy(char *dst, const char *src, size_t size)
 {
 	size_t	i;
 
+	i = 0;
 	if (!dst || !src)
 	{
 		return (0);
@@ -24,10 +25,10 @@ size_t	strlcpy(char *dst, const char *src, size_t size)
 	{
 		while (src[i] && i < size - 1)
 		{
-			dest[i] = src[i];
+			dst[i] = src[i];
 			i++;
 		}
-		dest[i] = '\0';
+		dst[i] = '\0';
 	}
 	while (src[i])
 	{
