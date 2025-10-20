86238f25b9594136537fad071a02f76a5f7f3194
Ryan Delepine <rydelepi@c1r3p9.42lehavre.fr>
2025-10-20 16:50:02 +0200

    modifs atoi

--- ft_atoi.c
+++ ft_atoi.c
@@ -6,11 +6,32 @@
 /*   By: rydelepi <rydelepi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/14 10:14:06 by rydelepi          #+#    #+#             */
-/*   Updated: 2025/10/20 09:26:50 by rydelepi         ###   ########.fr       */
+/*   Updated: 2025/10/20 16:49:44 by rydelepi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
+
+static int	too_long(const char *nptr_s, int sign)
+{
+	int	i;
+	int	j;
+
+	i = 0;
+	while (nptr_s[i] == '0')
+		i++;
+	j = 0;
+	while (nptr_s[i + j] >= '0' && nptr_s[i + j] <= '9')
+		j++;
+	if (j >= 20)
+	{
+		if (sign == 1)
+			return (-1);
+		else
+			return (0);
+	}
+	return (1);
+}
 
 int	ft_whitespace(char c)
 {
@@ -39,6 +60,8 @@ int	ft_atoi(char *str)
 			sign *= -1;
 		i++;
 	}
+	if (too_long(&nptr[i], sign) != 1)
+		return (too_long(&nptr[i], sign));
 	while (str[i] >= '0' && str[i] <= '9')
 	{
 		total = total * 10 + (str[i] - '0');
