7e2746965b42608f5fde73a6eaeabd414fa9e104
Ryan Delepine <rydelepi@c1r2p11.42lehavre.fr>
2025-10-23 16:26:06 +0200

    hi

--- ft_putnbr_fd.c
+++ ft_putnbr_fd.c
@@ -6,16 +6,11 @@
 /*   By: rydelepi <rydelepi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/14 09:14:09 by rydelepi          #+#    #+#             */
-/*   Updated: 2025/10/17 09:04:04 by rydelepi         ###   ########.fr       */
+/*   Updated: 2025/10/23 15:39:54 by rydelepi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
-
-void	ft_putchar_fd(char c, int fd)
-{
-	write(fd, &c, 1);
-}
 
 void	ft_putnbr_fd(int nb, int fd)
 {
