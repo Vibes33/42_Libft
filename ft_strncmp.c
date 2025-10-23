022aa360f0b02b70b4c59bfc759b692ad62fbdc3
Ryan Delepine <rydelepi@c1r2p11.42lehavre.fr>
2025-10-23 15:17:05 +0200

    modif

--- ft_strncmp.c
+++ ft_strncmp.c
@@ -6,7 +6,7 @@
 /*   By: rydelepi <rydelepi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/13 14:21:59 by rydelepi          #+#    #+#             */
-/*   Updated: 2025/10/23 15:13:00 by rydelepi         ###   ########.fr       */
+/*   Updated: 2025/10/23 15:16:58 by rydelepi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,8 +17,6 @@ int	ft_strncmp(const char *s1, const char *s2, size_t n)
 	size_t	i;
 
 	i = 0;
-	if (n <= 0)
-		return (0);
 	while ((s1[i] || s2[i]) && i < n)
 	{
 		if (s1[i] != s2[i])
